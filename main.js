dd55bff9221adb3a22a9ad38eef11bf56603a668
SCP-8530 <50393891+SCP-8530@users.noreply.github.com>
2019-08-13 13:12:24 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+
+bot.login(process.env.TOKEN);
+
+bot.on('ready', async () => {
+    console.log('bot connecter');
+    bot.user.setActivity("Observer Langitia");
+});
+
+let prefix = 'c!';
+
