b97671a751598eebc6376a329bde6d2bd471884c
SCP-8530 <WDgaster.gtp@gmail.com>
2019-08-14 00:21:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 
 let prefix = 'c!';
 
-bot.login('process.env.token')
+bot.login('NjEwMjAxNzQxNjEwNDUwOTQ0.XVLatg.VoibNE70MbR1rY_uvejpDD52-vY')
 
 bot.on('ready', async () => {
     console.log('bot connecter');
