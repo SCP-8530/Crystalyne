324357c9cf09c15f1e70e6d1a7c991aa6bafa4a6
SCP-8530 <WDgaster.gtp@gmail.com>
2019-08-13 22:55:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-
-bot.login(process.env.TOKEN);
+bot.login('NjEwMjAxNzQxNjEwNDUwOTQ0.XVLatg.VoibNE70MbR1rY_uvejpDD52-vY')
 
 bot.on('ready', async () => {
     console.log('bot connecter');
