6117b364d53069ca59bac4f0307183a08c10a812
SCP-8530 <WDgaster.gtp@gmail.com>
2019-08-14 23:43:05 -0400

    SCP-8530

---
+++ commands/pf.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    let event = random(0,1);
+    if (event==0) return message.channel.send("Tu lance une piece en l'air. quand tu la ratrape elle tombe sur **Face**")
+    if (event==1) return message.channel.send("Tu lance une piece en l'air. quand tu la ratrape elle tombe sur **Pile**")
+};
+
+module.exports.help = {
+    name: 'pf'
+};
+
+
+
+//penser a supprimer les messages hors code sauf si sais un repaire pour se retourver
