6f5c7b3831fd7938d5ac24e39a17c7fc1fa37e45
SCP-8530 <WDgaster.gtp@gmail.com>
2019-08-14 23:00:33 -0400

    Update maj.js

--- commands/maj.js
+++ commands/maj.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
     console.log('commande fonctionnelle');
-    return message.channel.send('ok -_-')
+    return message.channel.send('ok -_-');
 };
 
 module.exports.help = {
