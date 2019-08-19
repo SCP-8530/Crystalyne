c730632258fb4c419813dfd5eae585228c00f57e
SCP-8530 <WDgaster.gtp@gmail.com>
2019-08-19 12:14:57 -0400

    Update maj.js

--- commands/maj.js
+++ commands/maj.js
@@ -2,7 +2,14 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     console.log('commande fonctionnelle');
-    return message.channel.send('ok -_-');
+    let Here = message.guild.member(
+        message.mention.user.frist() || message.guild.member.get(args[0])
+    );
+    if(!Here) return message.channel.send('tu as oublier de mentionner here')
+    if(Here) Here.AddRole('610901457499193372')
+    message.channel.send('ok -_-');
+
+
 };
 
 module.exports.help = {
