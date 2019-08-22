7118c5534e33c6d658ecb3a9a478be1c3045faa4
SCP-8530 <WDgaster.gtp@gmail.com>
2019-08-21 22:15:22 -0400

    Update buche.js

--- commands/buche.js
+++ commands/buche.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
     //les chances
-    let prob = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    //let prob = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     //les differentes ressources
-    let bois = Math.floor((Math.random() * prob.length + 1))
+    let bois = Math.floor((Math.random() * /*prob.length*/ + 1))
 
     message.channel.send(`Tu as recuperer ${bois} de bois. Ses ressource se teleporte dans ton crystal`)
     let channellog = message.guild.channels.find('id', '604987895207428096');
