2332d77cb99a9be84560c808282aafebae1e2ecf
SCP-8530 <WDgaster.gtp@gmail.com>
2019-08-21 23:04:57 -0400

    Update recup.js

--- commands/recup.js
+++ commands/recup.js
@@ -5,11 +5,13 @@ module.exports.run = async (bot, message, args) => {
     let proba = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     let probb = [1, 2, 3, 4, 5];
     let probc = [1, 2, 3];
+    
     //les differentes ressources
     let vege = Math.floor((Math.random() * proba.length + 1));
     let bois = Math.floor((Math.random() * probb.length));
     let pierre = Math.floor((Math.random() * probc.length));
 
+    //reponce
     message.channel.send(`Tu as recuperer **${vege} de végétaux.** Tu as recuperer **${bois} de bois.** Tu as recuperer **${pierre} de pierre.** Ses ressource se teleporte dans ton crystal.`);
     let channellog = message.guild.channels.find('id', '604987895207428096');
     let utilisateur = message.member;
