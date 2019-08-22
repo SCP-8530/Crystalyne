42cee957ff6e5770c82c764cf98f5f4bd94b3be2
SCP-8530 <WDgaster.gtp@gmail.com>
2019-08-21 22:59:29 -0400

    Update recup.js

--- commands/recup.js
+++ commands/recup.js
@@ -6,16 +6,16 @@ module.exports.run = async (bot, message, args) => {
     let probb = [1, 2, 3, 4, 5];
     let probc = [1, 2, 3];
     //les differentes ressources
-    let vege = Math.floor((Math.random() * proba.length + 1))
-    let bois = Math.floor((Math.random() * probb.length))
-    let pierre = Math.floor((Math.random() * probc.length))
+    let vege = Math.floor((Math.random() * proba.length + 1));
+    let bois = Math.floor((Math.random() * probb.length));
+    let pierre = Math.floor((Math.random() * probc.length));
 
-    message.channel.send(`Tu as recuperer **${vege} de végétaux.**`, `Tu as recuperer **${bois} de bois.**`, `Tu as recuperer **${pierre} de pierre.**`, `Ses ressource se teleporte dans ton crystal.`)
+    message.channel.send(`Tu as recuperer **${vege} de végétaux.**`, `Tu as recuperer **${bois} de bois.**`, `Tu as recuperer **${pierre} de pierre.**`, `Ses ressource se teleporte dans ton crystal.`);
     let channellog = message.guild.channels.find('id', '604987895207428096');
-    let utilisateur = message.member
-    channellog.send(`!give-item ${utilisateur} ${vege} végétaux`)
-    channellog.send(`!give-item ${utilisateur} ${bois} bois`)
-    channellog.send(`!give-item ${utilisateur} ${pierre} végétaux`)
+    let utilisateur = message.member;
+    channellog.send(`!give-item ${utilisateur} ${vege} végétaux`);
+    channellog.send(`!give-item ${utilisateur} ${bois} bois`);
+    channellog.send(`!give-item ${utilisateur} ${pierre} végétaux`);
 };
 
 module.exports.help = {
