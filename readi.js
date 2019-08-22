7662c894f1b156daeaabc45f85f7211548b99527
SCP-8530 <WDgaster.gtp@gmail.com>
2019-08-22 18:27:46 -0400

    Delete readi.js

--- readi.js
+++
@@ -1,6 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports.run = async (bot, message, args) => {
-    let channeltest = message.guild.channels.find('id', '613126591203115149');
-    channeltest.send('je suis mis a jour')
-};
