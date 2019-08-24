0705ec70de76919c9cce0eecaa5c3e1558c5b105
SCP-8530 <WDgaster.gtp@gmail.com>
2019-08-24 15:21:51 -0400

    Update mine.js

--- commands/mine.js
+++ commands/mine.js
@@ -15,16 +15,16 @@ module.exports.run = async (bot, message, args) => {
 
     //logchannel
     let IDchannel = Math.floor((Math.random() * 9))
-    if (IDchannel==0) return {let channelID = 614242539888967738};
-    if (IDchannel==1) return {let channelID = 614242581177565210};
-    if (IDchannel==2) return {let channelID = 614242610562859041};
-    if (IDchannel==3) return {let channelID = 614242653839818771};
-    if (IDchannel==4) return {let channelID = 614242689109721106};
-    if (IDchannel==5) return {let channelID = 614242714040533002};
-    if (IDchannel==6) return {let channelID = 614242740254801969};
-    if (IDchannel==7) return {let channelID = 614242772400209931};
-    if (IDchannel==8) return {let channelID = 614242796798476308};
-    if (IDchannel==9) return {let channelID = 614242831829041162};
+    let channelID = [614242539888967738, 614242581177565210, 614242610562859041, 614242653839818771, 614242689109721106, 614242714040533002, 614242740254801969, 614242772400209931, 614242796798476308, 614242831829041162]
+    /*if (IDchannel==1) return {let channelID = 614242581177565210};c
+    if (IDchannel==2) return {let channelID = 614242610562859041};c
+    if (IDchannel==3) return {let channelID = 614242653839818771};c
+    if (IDchannel==4) return {let channelID = 614242689109721106};c
+    if (IDchannel==5) return {let channelID = 614242714040533002};c
+    if (IDchannel==6) return {let channelID = 614242740254801969};c
+    if (IDchannel==7) return {let channelID = 614242772400209931};c
+    if (IDchannel==8) return {let channelID = 614242796798476308};c
+    if (IDchannel==9) return {let channelID = 614242831829041162};c*/
     //reponce
     message.channel.send(`Tu as recuperer **${pierre} de pierre.** Tu as recuperer **${charbon} de charbon.** Tu as recuperer **${fer} de minerai de fer.** Tu as recuperer **${or} de minerai de or.** Ses ressource se teleporte dans ton crystal.`);
     let channellog = message.guild.channels.find('id', channelID);
