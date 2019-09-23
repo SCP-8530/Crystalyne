74beed41c139d1c977f99278be921ccf397bf808
SCP-8530 <WDgaster.gtp@gmail.com>
2019-09-23 17:58:17 -0400

    Update vide.js

--- commands/vide.js
+++ commands/vide.js
@@ -11,14 +11,14 @@ module.exports.run = async (bot, message, args) => {
     //clear
     let logchannel = message.guild.channels.find('id', '604987895207428096');
     
-    logchannel.send(`!take-item ${user_void_inv} 10000 bois`)
-    logchannel.send(`!take-item ${user_void_inv} 10000 pierre`)
-    logchannel.send(`!take-item ${user_void_inv} 10000 minerai de fer`)
-    logchannel.send(`!take-item ${user_void_inv} 10000 charbon`)
-    logchannel.send(`!take-item ${user_void_inv} 10000 vegeteux`)
-    logchannel.send(`!take-item ${user_void_inv} 10000 minerai d'or`)
-    logchannel.send(`!take-item ${user_void_inv} 10000 lingo d'or`)
-    logchannel.send(`!take-item ${user_void_inv} 10000 lingo de fer`)
+    logchannel.send(`!take-item ${user_void_inv} 10000 bois`);
+    logchannel.send(`!take-item ${user_void_inv} 10000 pierre`);
+    logchannel.send(`!take-item ${user_void_inv} 10000 minerai de fer`);
+    logchannel.send(`!take-item ${user_void_inv} 10000 charbon`);
+    logchannel.send(`!take-item ${user_void_inv} 10000 vegeteux`);
+    logchannel.send(`!take-item ${user_void_inv} 10000 minerai d'or`);
+    logchannel.send(`!take-item ${user_void_inv} 10000 lingo d'or`);
+    logchannel.send(`!take-item ${user_void_inv} 10000 lingo de fer`);
 
 };
 
