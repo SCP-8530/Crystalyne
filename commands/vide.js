53c8d5d4d8575af39719a6826021657f73cf1600
SCP-8530 <50393891+SCP-8530@users.noreply.github.com>
2019-09-23 14:32:22 -0400

    Update vide.js

--- commands/vide.js
+++ commands/vide.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    //let user_void_inv = 
+    let user_void_inv = message.mentions.members.first();
 };
 
 module.exports.help = {
