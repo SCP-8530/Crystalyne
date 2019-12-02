cb484afa98ca46e8971bcb46cff2069994edc17b
SCP-8530 <WDgaster.gtp@gmail.com>
2019-12-02 18:46:54 -0500

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -4,13 +4,31 @@ module.exports.run = async (bot, message, args) => {
     let question = args.slice(0).join(' ')
     
     //diferente reponse
-    let errorEmbed = new Discord.RichEmbed()
-         .setDescription('8Ball')
-         .setColor('#FFFFFF')
-         .addField('Pas de question', 'Veuiller posser une question a 8Ball');
-    
-    if (!question) return message.channel.send(errorEmbed)
-    if (question == help) else 
+    let errorEmbed = new Discord.RichEmbed() 
+        .setDescription('8Ball')
+        .setColor('#000000')
+        .addField('Pas de question', 'Veuiller posser une question a 8Ball');
+    let helpEmbed = new Discord.RichEmbed()
+        .setDescription('Help 8ball')
+        .setColor('#000000')
+        .addField('','Pause une question a 8ball qui peut se repondre par oui ou par non.');
+    let repEmbed = new Discord.RichEmbed()
+        .setDescription('8Ball')
+        .setColor('#000')
+        .addField(question, reponse);
+    //reponces de 8ball
+    let variable8ball = Math.floor((Math.random()* 6));
+
+    if (variable8ball == 0) let reponse = 'oui';
+    if (variable8ball == 1) let reponse = 'non';
+    if (variable8ball == 2) let reponse = 'peut-etre';
+    if (variable8ball == 3) let reponse = 'je ne sais pas';
+    if (variable8ball == 4) let reponse = 'surment';
+    if (variable8ball == 5) let reponse = 'il y a peut de chance';
+    //comportement
+    if (!question) return message.channel.send(errorEmbed);
+    if (question == help) message.channel.send(helpEmbed) 
+    else message.channel.send(repEmbed);
          
 };
 
